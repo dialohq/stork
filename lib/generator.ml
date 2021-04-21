c6f825b9824d07d21d01c92c98a47923745a4ad0
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2021-04-21 11:23:45 +0200

    change impl when targetting rescript (#5)
    * change impl when targetting rescript
    
    * disable warning 33 in generated impl
    
    * refactor decoder maker functions

--- lib/generator.ml
+++ lib/generator.ml
@@ -97,8 +97,9 @@ let make_convert_to_latest_fns = function
     in
     aux ~acc:[ latest_converter ] tail |> List.rev
 
-let get_version_from_json =
-  {|
+let make_get_version_from_json = function
+  | Config.Native ->
+    {|
 let get_version_from_json = function
   | `Assoc fields ->
     let version =
@@ -114,6 +115,22 @@ let get_version_from_json = function
   | _ ->
     invalid_arg "The parsed JSON should be an object."
 |}
+  | Config.Rescript ->
+    {|
+let get_version_from_json json =
+  let toInteger : float -> int option = fun value ->
+    if Js.Float.isFinite value && Js.Math.floor_float value == value 
+    then Some (int_of_float value)
+    else None
+  in let getVersionField obj = Js.Dict.get obj "version"
+  in let obj = Js.Json.decodeObject json
+  in let versionJson = Belt.Option.flatMap obj getVersionField
+  in let versionNumber = Belt.Option.flatMap versionJson Js.Json.decodeNumber
+  in let version = Belt.Option.flatMap versionNumber toInteger in
+  match version with 
+  | Some version -> version 
+  | None -> invalid_arg "The parsed JSON should be an object with a `version` field of type int."
+|}
 
 let get_version =
   {|let get_version s = 
@@ -123,9 +140,8 @@ let get_version_from_lexbuf =
   {|let get_version_from_lexbuf p lb = 
   get_version_from_json (Yojson.Safe.from_lexbuf p lb)|}
 
-let make_main_of_string ~impl_kind ~prefix ~main_type =
-  let main_type_of_string = [%string "$(main_type)_of_string"] in
-  function
+let make_decode_main ~impl_kind ~prefix ~fn_name ~arg ~fn_sig ~match_version
+  = function
   | [] ->
     assert false
   | latest_version :: tail ->
@@ -134,51 +150,58 @@ let make_main_of_string ~impl_kind ~prefix ~main_type =
         ~f:(fun version ->
           [%string
             "  | %i$version -> $(name_convert_to_latest version) \
-             ($(Upgrader.name_impl_module impl_kind prefix \
-             version).$main_type_of_string s)"])
+             ($(Upgrader.name_impl_module impl_kind prefix version).$fn_name \
+             $arg)"])
         tail
       |> String.concat ~sep:"\n"
     in
     ( [%string
         {|
-let $main_type_of_string s = match (get_version s) with
-  | %i$latest_version -> Json.$main_type_of_string s
+let $fn_name $arg = $match_version with
+  | %i$latest_version -> Json.$fn_name $arg
 $version_matches
   | v -> invalid_arg ("Unknown document version: " ^ "'" ^ (string_of_int v) ^ "'")
 |}]
-    , [%string "val $main_type_of_string: string -> Types.$main_type"] )
+    , [%string "val $fn_name: $fn_sig"] )
 
-let make_read_main ~impl_kind ~prefix ~main_type =
-  let read_main_type = [%string "read_$main_type"] in
-  function
-  | [] ->
-    assert false
-  | latest_version :: tail ->
-    let version_matches =
-      List.map
-        ~f:(fun version ->
-          [%string
-            "  | %i$version -> $(name_convert_to_latest version) \
-             ($(Upgrader.name_impl_module impl_kind prefix \
-             version).$read_main_type p lb)"])
-        tail
-      |> String.concat ~sep:"\n"
-    in
-    (* todo change implementation of lexbuf reuse *)
-    ( [%string
-        {|
-let $read_main_type p lb = let Yojson.{ lnum; fname; _ } = p in 
+let make_main_of_string ~main_type =
+  let arg = "s" in
+  make_decode_main
+    ~fn_name:[%string "$(main_type)_of_string"]
+    ~arg
+    ~fn_sig:[%string "string -> Types.$main_type"]
+    ~match_version:[%string "match (get_version $arg)"]
+
+let make_read_main ~main_type ~impl_kind =
+  let arg, fn_sig, match_version =
+    match impl_kind with
+    | Config.Rescript ->
+      let arg = "json" in
+      let fn_sig = [%string "Types.$main_type Atdgen_codec_runtime.Decode.t"] in
+      let match_version = [%string "match (get_version_from_json $arg)"] in
+      arg, fn_sig, match_version
+    | Config.Native ->
+      let arg = "p lb" in
+      let fn_sig =
+        [%string "Yojson.Safe.lexer_state -> Lexing.lexbuf -> Types.$main_type"]
+      in
+      let match_version =
+        [%string
+          {|let Yojson.{ lnum; fname; _ } = p in 
   let new_p = Yojson.init_lexer ?fname ~lnum () in
   let ic = open_in_bin (Option.get fname) in
   let new_lb = Lexing.from_channel ic
-  in match (get_version_from_lexbuf new_p new_lb) with
-  | %i$latest_version -> Json.$read_main_type p lb
-$version_matches
-  | v -> invalid_arg ("Unknown document version: " ^ "'" ^ (string_of_int v) ^ "'")
-|}]
-    , [%string
-        "val $read_main_type: Yojson.Safe.lexer_state -> Lexing.lexbuf -> \
-         Types.$main_type"] )
+  in match (get_version_from_lexbuf new_p new_lb)|}]
+        (* todo change implementation of lexbuf reuse *)
+      in
+      arg, fn_sig, match_version
+  in
+  make_decode_main
+    ~fn_name:[%string "read_$main_type"]
+    ~arg
+    ~fn_sig
+    ~impl_kind
+    ~match_version
 
 let make_string_of_main main_type =
   let intf =
@@ -186,6 +209,50 @@ let make_string_of_main main_type =
   in
   let impl = [%string "let string_of_$main_type = Json.string_of_$main_type"] in
   impl, intf
+
+let make_write_main main_type =
+  let intf =
+    [%string
+      "val write_$main_type: Types.$main_type Atdgen_codec_runtime.Encode.t"]
+  in
+  let impl = [%string "let write_$main_type = Json.write_$main_type"] in
+  impl, intf
+
+let make_read_and_write_main ~prefix ~impl_kind ~main_type versions =
+  match impl_kind with
+  | Config.Native ->
+    let string_of_main_impl, string_of_main_intf =
+      make_string_of_main main_type
+    in
+    let read_main_impl, read_main_intf =
+      make_read_main ~prefix ~impl_kind ~main_type versions
+    in
+    let main_of_string_impl, main_of_string_intf =
+      make_main_of_string ~prefix ~impl_kind ~main_type versions
+    in
+    let impl_list =
+      [ make_get_version_from_json impl_kind
+      ; get_version
+      ; get_version_from_lexbuf
+      ; string_of_main_impl
+      ; main_of_string_impl
+      ; read_main_impl
+      ]
+    in
+    let intf_list =
+      [ main_of_string_intf; string_of_main_intf; read_main_intf ]
+    in
+    impl_list, intf_list
+  | Config.Rescript ->
+    let write_main_impl, write_main_intf = make_write_main main_type in
+    let read_main_impl, read_main_intf =
+      make_read_main ~prefix ~impl_kind ~main_type versions
+    in
+    let impl_list =
+      [ make_get_version_from_json impl_kind; write_main_impl; read_main_impl ]
+    in
+    let intf_list = [ write_main_intf; read_main_intf ] in
+    impl_list, intf_list
 
 let make_upgraders ?(impl_kind = Config.Native) ?output_prefix = function
   | [] ->
@@ -297,18 +364,12 @@ let make_upgraders ?(impl_kind = Config.Native) ?output_prefix = function
       [%string "module Json: module type of $newest_module_impl"]
     in
     let desc_versions = List.rev file_versions in
-    let main_of_string_impl, main_of_string_intf =
-      make_main_of_string ~impl_kind ~prefix ~main_type desc_versions
-    in
     let convert_to_latest_fns = make_convert_to_latest_fns version_pairs in
-    let string_of_main_impl, string_of_main_intf =
-      make_string_of_main main_type
-    in
-    let read_main_impl, read_main_intf =
-      make_read_main ~impl_kind ~prefix ~main_type desc_versions
+    let read_write_main_impl_list, read_write_main_intf_list =
+      make_read_and_write_main ~prefix ~impl_kind ~main_type desc_versions
     in
     let open_std = "open StdLabels" in
-    let disable_warnings_impl = {|[@@@ocaml.warning "-32-44"]|} in
+    let disable_warnings_impl = {|[@@@ocaml.warning "-32-33-44"]|} in
     let disable_warnings_intf = {|[@@@ocaml.warning "-34"]|} in
     let upgraders =
       Upgrader.
@@ -316,10 +377,7 @@ let make_upgraders ?(impl_kind = Config.Native) ?output_prefix = function
           intf_list =
             types_module_sig
             ::
-            json_module_sig
-            ::
-            main_of_string_intf
-            :: string_of_main_intf :: read_main_intf :: upgraders.intf_list
+            json_module_sig :: (read_write_main_intf_list @ upgraders.intf_list)
         ; impl_list =
             disable_warnings_impl
             ::
@@ -329,15 +387,9 @@ let make_upgraders ?(impl_kind = Config.Native) ?output_prefix = function
             ::
             json_module
             ::
-            get_version_from_json
-            ::
-            get_version
-            ::
-            get_version_from_lexbuf
-            ::
             (upgraders.impl_list
             @ convert_to_latest_fns
-            @ [ string_of_main_impl; main_of_string_impl; read_main_impl ])
+            @ read_write_main_impl_list)
         ; user_intf_list = disable_warnings_intf :: upgraders.user_intf_list
         }
     in
