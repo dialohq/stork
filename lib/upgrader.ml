db392b038b07b53bef4bd2df886897775c0a722f
Wojtek Czekalski <me@wczekalski.com>
2021-06-22 22:28:03 +0200

    Revert "Remove double declaration of type $converter"
    This reverts commit 724c828464b767752deb26baeb695b4e1cc78742.

--- lib/upgrader.ml
+++ lib/upgrader.ml
@@ -658,6 +658,7 @@ let make ~prefix ~old_file ~old_file_version ~new_file ~new_file_version =
       [%string
         {|
 include $user_fns_module
+type $converter = $upgrader_t.$module_name.$converter
 |}]
     in
     let intf_header =
