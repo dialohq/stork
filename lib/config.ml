1d307b7ba014d3a9e0299dcaaf642f559554d06c
Wojtek Czekalski <me@wczekalski.com>
2021-07-24 15:14:01 +0200

    Remove unused stork config dir

--- lib/config.ml
+++ lib/config.ml
@@ -1,27 +1,24 @@
 let home =
-  let env_var = match Sys.os_type with "Unix" -> "HOME" | _ -> "APPDATA" in
-  Sys.getenv_opt env_var
-  |> Option.to_result ~none:(Stork_error.missing_env env_var)
+  try Ok (Sys.getenv "HOME") with
+  | Not_found -> (
+    try Ok (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir with
+    | Unix.Unix_error _
+    | Not_found ->
+      if Sys.win32 then
+        try Ok (Sys.getenv "AppData") with
+        | Not_found -> Error (Stork_error.missing_env "AppData")
+      else
+        Error (Stork_error.missing_env "HOME"))
 
 let default_cache_dir =
   Result.map
     (fun home -> "stork" |> Filename.concat ".cache" |> Filename.concat home)
     home
 
-let default_config_dir =
-  Result.map
-    (fun home -> "stork" |> Filename.concat ".config" |> Filename.concat home)
-    home
-
 let cache_dir =
   Sys.getenv_opt "STORK_CACHE_DIR"
   |> Option.map Result.ok
   |> Option.value ~default:default_cache_dir
-
-let config_dir =
-  Sys.getenv_opt "STORK_CONFIG_DIR"
-  |> Option.map Result.ok
-  |> Option.value ~default:default_config_dir
 
 type impl_kind =
   | Native
